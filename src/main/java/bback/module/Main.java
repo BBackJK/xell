b4f7af325f413c38e7b8c87c7f7db81a6cc3f398
bbacks <wnsrud6839@gmail.com>
2023-08-28 17:44:25 +0900

    💫 style : apply sonarlint
    - Main 클래스 sysout to log 로 수정

--- src/main/java/bback/module/Main.java
+++ src/main/java/bback/module/Main.java
@@ -1,7 +1,10 @@
 package bback.module;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        log.debug("bback excel module on");
     }
 }
